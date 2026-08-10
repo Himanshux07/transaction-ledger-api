2c7ee582e28a730434d26ef15b0e96185e7491d0
Himanshux07 <annikumar997@gmail.com>
2026-08-10 18:19:07 +0530

    feat: create User model with password hashing and JWT authentication methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -30,12 +30,12 @@ const userSchema = new mongoose.Schema({
     },
 },{timestamps:true})
 
-userSchema.pre("save",async function(next){
-    if(this.isModified("password")){
-        this.password = await bcrypt.hash(this.password,10);
+userSchema.pre("save", async function () {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
     }
-    next();
-})
+});
+
 
 userSchema.methods.comparePassword = async function(password){
     return await bcrypt.compare(password,this.password);
